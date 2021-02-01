9962d96b82cc2186f3883541201c6ed6275d7521
Lucas Urriste <56521332+Urriste@users.noreply.github.com>
2021-02-01 15:56:50 -0300

    Add files via upload

--- Piedra Papel o Tijera Visual/js/Piedra Papel O Tijera (atado con alambre).js
+++ Piedra Papel o Tijera Visual/js/Piedra Papel O Tijera (atado con alambre).js
@@ -1,105 +1,119 @@
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
 let btnJugar = document.getElementById('btn-jugar');
-let ganadores = document.getElementById('ganador')
-let btn1 = document.getElementById('btn1');
-let btn2 = document.getElementById('btn2');
+let ganadores = document.getElementById('ganador');
 let player1 = document.getElementById('eleccion1');
 let player2 = document.getElementById('eleccion2');
-
-let jugador1 = "";
-let jugador2 ="";
-
-
-
-let eleccion1 = "";
-let eleccion2 = "";
+let reset = document.getElementById('reset')
+let resultado = "";
+let jugador1 = {nombre:"", eleccion:""};
+let jugador2 = {nombre:"", eleccion:""};
 
 
 
-let numero = (Math.floor(Math.random ()*3)+1);
-let numero2 = (Math.floor(Math.random ()*3)+1);
 
+function eleccion (){
+	let n = (Math.floor(Math.random ()*3)+1);
 
-
-switch (numero){
-	case 1:
-		eleccion1 = "piedra";
-	break;
-	case 2: 
-		eleccion1 = "papel";
-	break;
-	case 3:
-		eleccion1 ="tijera";
-	break;
-	default:
-		alert("flashaste");
-	break
-}
-
-
-switch (numero2){
-	case 1:
-		eleccion2 = "piedra";
-	break;
-	case 2: 
-		eleccion2 = "papel";
-	break;
-	case 3:
-		eleccion2 ="tijera";
-	break;
-	default:
-		alerta("flashaste");
-	break
-}
-
-
-
-btn1.addEventListener('click', ()=>{
-	jugador1 = input1.value;
-	player1.innerHTML = `El/la jugador/a 1 es: ${jugador1}`;
-} )
-
-btn2.addEventListener('click', ()=>{
-	jugador2 = input2.value;
-	player2.innerHTML =  `El/la jugador/a 2 es: ${jugador2}`;
-} )
-
-function ganador (eleccion1, eleccion2) {
-	//Casos si Jugador 1 tiene piedra//
-	if ((eleccion1 == "piedra") && (eleccion2 == "papel")){
-		ganadores.innerHTML = `El ganador es: ${jugador2} teniendo ${eleccion2}`;
+	if (n == 1){
+		return "piedra";
 	}
 
-	if ((eleccion1 == "piedra") &&(eleccion2 == "tijera")){
-		ganadores.innerHTML = `El ganador es: ${jugador1} teniendo ${eleccion1}`;
+	if (n == 2){
+		return "papel";
+	}
+
+	if (n == 3){
+		return "tijera";
+	}
+}
+
+
+
+function ganador () {
+//Casos si Jugador 1 tiene piedra//
+	if ((jugador1.eleccion == "piedra") && (jugador2.eleccion == "papel")){
+		resultado = jugador2.nombre;
+	}
+
+	if ((jugador1.eleccion == "piedra") &&(jugador2.eleccion2 == "tijera")){
+		resultado = jugador1.nombre;
+	}
+
+//Casos si el jugador 2 tiene piedra//
+
+	if ((jugador2.eleccion == "piedra") && (jugador1.eleccion == "papel")){
+		resultado = jugador1.nombre;
+	}
+
+	if ((jugador2.eleccion == "piedra") &&(jugador1.eleccion == "tijera")){
+		resultado = jugador2.nombre;
 	}
 
 //Casos si Jugador 1 tiene papel//
-	if ((eleccion1 == "papel") && (eleccion2 == "piedra")){
-		ganadores.innerHTML = `El ganador es: ${jugador1} teniendo ${eleccion1}`;
+	if ((jugador1.eleccion == "papel") && (jugador2.eleccion2 == "piedra")){
+		resultado = jugador1.nombre;
 	}
 
-	if((eleccion1 == "papel") && (eleccion2 == "tijera")){
-		ganadores.innerHTML = `El ganador es: ${jugador2} teniendo ${eleccion2}`;
+	if((jugador1.eleccion == "papel") && (jugador2.eleccion == "tijera")){
+		resultado = jugador2.nombre;
 	}
+
+//Casos si el jugador 2 tiene papel//
+	if ((jugador2.eleccion == "papel") && (jugador1.eleccion == "piedra")){
+		resultado = jugador2.nombre;
+	}
+
+	if((jugador2.eleccion == "papel") && (jugador1.eleccion == "tijera")){
+		resultado = jugador1.nombre;
+	}
+
 
 
 //Casos si Jugador 1 tiene tijeras//
-	if ((eleccion1 == "tijera") && (eleccion2 == "papel")){
-		ganadores.innerHTML = `El ganador es: ${jugador1} teniendo ${eleccion1}`;
+	if ((jugador1.eleccion == "tijera") && (jugador2.eleccion == "papel")){
+		resultado = jugador1.nombre;
 	}
 
-	if ((eleccion1 == "tijera") && (eleccion2 == "piedra")){
-		ganadores.innerHTML = `El ganador es: ${jugador2} teniendo ${eleccion2}`;
+	if ((jugador1.eleccion == "tijera") && (jugador2.eleccion == "piedra")){
+		resultado = jugador2.nombre;
 	}
 
-	if (eleccion1 == eleccion2){
-		ganadores.innerHTML = `Es un empate! Ambos tuvieron  ${eleccion1}`;
-	}
+
+
+//Casos si Jugador 2 tiene tijeras//
+if ((jugador2.eleccion == "tijera") && (jugador1.eleccion == "papel")){
+	resultado = jugador2.nombre;
+}
+
+if ((jugador2.eleccion == "tijera") && (jugador1.eleccion == "piedra")){
+	resultado = jugador1.nombre;
+}
+
+
+
 }
 
 btnJugar.addEventListener('click', ()=>{
 
-	ganador(eleccion1,eleccion2)
+	jugador1.nombre = input1.value;
+	jugador2.nombre = input2.value;
+
+
+
+	jugador1.eleccion = eleccion();
+	jugador2.eleccion = eleccion();
+
+	ganador();
+	
+	
+	ganadores.classList.replace('none', 'active');
+	if (jugador1.eleccion != jugador2.eleccion){
+		ganadores.innerHTML = `El/la ganador/a es ${resultado}`;
+	}else{
+		ganadores.innerHTML = "Es un empate!"
+	}
+	
+	reset.style.display = 'block';
+	
 });
